1e4293f31975274fb96f94f83aa9a1644746f959
Renan Palmeira <renan@forleven.com>
2016-02-02 23:47:05 -0200

    First commit

---
+++ hello.cpp
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <curl/curl.h>
+#include "libapi/libapi.h"
+
+int main(void) {
+	return 0;
+}
---
+++ include/libapi/libapi.h
@@ -0,0 +1,10 @@
+#ifndef INCLUDE_LIBAPI_LIBAPI_H_
+#define INCLUDE_LIBAPI_LIBAPI_H_
+
+#include <curl/curl.h>
+
+namespace libapi {
+	int get(char url);
+}
+
+#endif
