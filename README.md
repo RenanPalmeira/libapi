a0a254350e46e2cd4346fd7571cdb6fbe60eaf06
Renan Palmeira <renan@forleven.com>
2016-02-03 23:52:42 -0200

    Test in g++ compile .h

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # libapi
+
+
+http://stackoverflow.com/questions/228684/how-to-declare-a-structure-in-a-header-that-is-to-be-used-by-multiple-files-in-c
--- include/libapi/libapi.h
+++ include/libapi/libapi.h
@@ -2,9 +2,16 @@
 #define INCLUDE_LIBAPI_LIBAPI_H_
 
 #include <curl/curl.h>
+#include <string>
 
 namespace libapi {
+	struct Response {
+		int code;
+		std::string body;
+	} Response;
 	int get(char url);
 }
+
+int egg();
 
 #endif
